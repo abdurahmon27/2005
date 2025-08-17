5b7fe46188dc9bf8dbb23368dd58d7a069175538
Abdurahmon Mamadiyorov <bekzotovich12@gmail.com>
2025-08-17 10:54:41 +0500

    fix: def text

--- src/app/garbage/page.tsx
+++ src/app/garbage/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const GarbagePage = () => {
   return (
     <div className="container h-[50vh] flex items-center justify-center">
-      {"This page is under constructor :)"}
+      {"This page is under construction :)"}
     </div>
   );
 };
